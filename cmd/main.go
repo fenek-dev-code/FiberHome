4cc9073b6531586b4119cd66348893d4d6916a93
fenek_dev <sahasubbota@gmail.com>
2025-12-28 01:33:29 +0300

    Completed HomeWork

--- cmd/main.go
+++ cmd/main.go
@@ -4,11 +4,15 @@ import (
 	"go-fiber/home/config"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/recover"
 )
 
 func main() {
 	config.LoadEnv()
 	_ = config.LoadConfigEnv()
-	// Application entry point
 	app := fiber.New()
+
+	app.Use(recover.New())
+
+	app.Listen(":3000")
 }
